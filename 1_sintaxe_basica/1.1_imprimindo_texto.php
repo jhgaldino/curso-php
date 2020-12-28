888d7daeddbec87875785a36e7f89b93bd0d706f
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2020-12-28 14:29:34 -0300

    imprimindo valores

---
+++ 1_sintaxe_basica/1.1_imprimindo_texto.php
@@ -0,0 +1,10 @@
+<?php
+//ola mundo php
+echo"Ola mundo!";
+//a variavel pode ser impressa de diversas maneiras diferentes
+//php pode ser misturado com html
+    $nome = "Fulano"
+    echo "Meu nome eh" . $nome<br/>;
+    echo "Meu nome eh" ($nome)<br/>;
+//pode ser imprimindo usando print tambem
+print "Ola mundo"
