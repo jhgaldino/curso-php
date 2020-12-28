44bcad25a031d3124045cbe512e650e1ca52e5d8
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2020-12-28 14:46:31 -0300

    variaveis e comentarios

---
+++ 1_sintaxe_basica/1.2_variaveis_e_comentarios.php
@@ -0,0 +1,14 @@
+<?php
+    #php eh uma linguagem case sensitive
+    #declarar variavel
+    $_nomecompleto = "Fulano de Tal";
+    $idade = 20;
+    $peso = 30.3;
+    #retornar o tipo de variavel
+    echo gettype($_nomecompleto);
+
+    #valores boleanos
+    $valido = true;
+    echo($valido)
+    #nao retona nada se for false, 1 se for true
+?>
