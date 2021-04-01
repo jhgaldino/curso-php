e5dbd0c7b8a06b88619a4a1c5b6de253c603866b
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2021-04-01 13:36:50 -0300

    1.7 Arrays

--- 1_sintaxe_basica/1.7_array.php
+++ 1_sintaxe_basica/1.7_array.php
@@ -1,5 +1,5 @@
 <?php
-$nomes = array{"ana","julia","gabi","felipe","fernanda","rodolfo"};
+$nomes = array["ana","julia","gabi","felipe","fernanda","rodolfo"];
 //exibir apenas valores especificos da array, começando sempre na posição 0
 echo($nomes[1],$nomes[2]);
 //é possivel acessar os valores da array usando laços de repeticao
